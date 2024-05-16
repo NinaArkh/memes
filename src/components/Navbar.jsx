96c90cef4082b6dc4a9edca57dba4aae8086c24b
NinaArkh <nina.arkhipova.16@gmail.com>
2024-05-16 18:16:54 +0300

    build version

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Smiley from './smile_c2.png'
 
 const Navbar = () => {
   
   return (
     <nav>
-      <img src="./smile_c2.png" alt="smile emoji" />
+      <img src={Smiley} alt="smile emoji" />
       <h1 className="title"> Meme Generator </h1>
       <p className='sidenote'> &#169; Powered by React </p>
     </nav>
